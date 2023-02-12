31a83aae6aa6e35beed77f5a5f5d72e3bd7c15ed
LifeOfCoding <LifeOfCoding@gmail.com>
2023-02-12 14:30:00 -0500

    Created new scroll effect on homepage + showcase section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
 // import { signIn, signOut, useSession } from "next-auth/react";
 
 import Layout from "../components/Main";
@@ -11,35 +12,88 @@ const Home: NextPage = () => {
   return (
     <Layout title="Home">
       <main className="relative z-10 flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Hi, I&apos;m Life<span className="text-hot-pink">Of</span>
-            Coding
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="about"
-            >
-              <h3 className="text-2xl font-bold">About Me →</h3>
-              <div className="text-lg">A little about me and my goals</div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="projects"
-            >
-              <h3 className="text-2xl font-bold">Projects →</h3>
-              <div className="text-lg">
-                Checkout what kind of projects I&aps;ve been working on lately.
-              </div>
-            </Link>
-          </div>
-          {/* <div className="flex flex-col items-center gap-2">
+        <div className="container mt-[30vh] flex h-[200vh] flex-col items-center justify-start px-4 py-6">
+          <div className="sticky top-20 flex flex-col items-center justify-center gap-12 px-4 py-6">
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              Hi, I&apos;m Life<span className="text-hot-pink">Of</span>
+              Coding
+            </h1>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+              <Link
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                href="about"
+              >
+                <h3 className="text-2xl font-bold">About Me →</h3>
+                <div className="text-lg">A little about me and my goals</div>
+              </Link>
+              <Link
+                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                href="projects"
+              >
+                <h3 className="text-2xl font-bold">Projects →</h3>
+                <div className="text-lg">
+                  Checkout what kind of projects I&aps;ve been working on
+                  lately.
+                </div>
+              </Link>
+            </div>
+            {/* <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
             <AuthShowcase />
           </div> */}
+          </div>
+        </div>
+        <div className="over min-h-screen w-full bg-black/50 text-center text-white">
+          <div className="m-2">
+            <h1 className="mb-5 text-2xl">Showcase</h1>
+
+            <div className="mt-2 grid grid-cols-1 gap-4 md:grid-cols-2">
+              <div className="group relative">
+                <a
+                  href="https://github.com/lifeofcoding/desktop-chatgpt"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src="/desktop-chatgpt.png"
+                    width="0"
+                    height="0"
+                    sizes="100vw"
+                    style={{ width: "100%", height: "auto" }}
+                    alt="Desktop ChatGPT"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity group-hover:!opacity-100">
+                    <p>Desktop ChatGPT</p>
+                  </div>
+                </a>
+              </div>
+              <div className="group relative">
+                <a
+                  href="https://github.com/lifeofcoding/AutoNFT"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src="/autonft.png"
+                    width="0"
+                    height="0"
+                    sizes="100vw"
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    alt="Auto NFT"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity group-hover:!opacity-100">
+                    <p>AutoNFT</p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    variants: {
+      extend: {
+        fontWeight: ["responsive", "hover", "focus"],
+        opacity: ["hover"],
+        borderColor: ["hover", "focus"],
+        margin: ["first", "last"],
+        backgroundColor: ["odd", "even"],
+        scale: ["hover", "active", "group-hover"],
+      },
+    },
     extend: {
       colors: {
         "hot-purple": "#2e026d",
