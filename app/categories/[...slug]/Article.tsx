2e2e4c7cf955c015c8d70e1a6cd562e09456c342
LifeOfCoding <LifeOfCoding@gmail.com>
2023-08-19 10:45:01 -0400

    Add author avatar

--- app/categories/[...slug]/Article.tsx
+++ app/categories/[...slug]/Article.tsx
@@ -14,7 +14,9 @@ export async function Article({ article }: { article: string }) {
     select: {
       title: true,
       content: true,
-      author: true,
+      author: {
+        select: { name: true, email: true, image: true },
+      },
       createdAt: true,
     },
   });
@@ -26,11 +28,22 @@ export async function Article({ article }: { article: string }) {
   return (
     <div className="relative">
       <div className="font-semibold text-5xl p-5 mb-5 mt-10">{page.title}</div>
-      <div className="m-5 p-2 bg-background border-foreground border rounded">
-        Author: {page.author.name || page.author.email} | Published:{" "}
-        {new Date(page.createdAt).toLocaleDateString()}
+      <div className="mx-5 mt-5 p-2 bg-background border-slate-500 border rounded flex items-center">
+        <div className="rounded-full mr-2">
+          <img
+            src={page.author.image}
+            alt={page.author.name || page.author.email}
+            width="auto"
+            height="45"
+            className="rounded-full h-10 w-10"
+          />
+        </div>
+        <div className="text-sm">
+          Author: {page.author.name || page.author.email} | Published:{" "}
+          {new Date(page.createdAt).toLocaleDateString()}
+        </div>
       </div>
-      <div className="article p-5">
+      <div className="article px-5 pb-5">
         <Markdown>{page.content}</Markdown>
       </div>
 
